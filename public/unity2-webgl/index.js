d3db4fe51d910a3ad935beb86042bd8bb34f8c9f
Harshit Siwach <37680927+harshitsiwach@users.noreply.github.com>
2025-05-28 02:40:28 +0530

    updated with new Unity build

--- public/unity2-webgl/index.js
+++ public/unity2-webgl/index.js
@@ -36,11 +36,11 @@ window.addEventListener("load", function () {
   }
 
   var buildUrl = "Build";
-  var loaderUrl = buildUrl + "/Chefton.loader.js";
+  var loaderUrl = buildUrl + "/WebGl.loader.js";
   var config = {
-    dataUrl: buildUrl + "/Chefton.data",
-    frameworkUrl: buildUrl + "/Chefton.framework.js",
-    codeUrl: buildUrl + "/Chefton.wasm",
+    dataUrl: buildUrl + "/WebGl.data",
+    frameworkUrl: buildUrl + "/WebGl.framework.js",
+    codeUrl: buildUrl + "/WebGl.wasm",
     streamingAssetsUrl: "StreamingAssets",
     companyName: "RuptureLabs",
     productName: "Chefton",
@@ -63,6 +63,7 @@ window.addEventListener("load", function () {
     document.getElementsByTagName('head')[0].appendChild(meta);
   }
 
+  canvas.style.background = "url('" + buildUrl + "/WebGl.jpg') center / cover";
   loadingBar.style.display = "block";
 
   var script = document.createElement("script");
